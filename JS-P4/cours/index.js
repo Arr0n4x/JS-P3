7805322124bdaa272a0c100a26883bc7072f86fb
Raphael Monneron <rglmonneron@gmail.com>
2021-11-08 11:55:14 +0100

    second commit

---
+++ JS-P4/cours/index.js
@@ -0,0 +1,43 @@
+// XMLHttpRequest
+
+function reqListener() {
+  console.log(this.responseText);
+}
+
+let req = new XMLHttpRequest();
+req.onload = reqListener;
+// req.open("get", "data.txt", true);
+// req.open("get", "data.json", true);
+// req.open("get", "https://api.blablagues.net/?rub=blagues", true);
+// req.send();
+
+//-------------------------
+// FETCH
+//-------------------------
+
+// fetch("monlien", "object d'options")
+//   .then((response) => {
+//     // console.log(response)
+//   })
+//   .catch((err) => console.log(err));
+
+// fetch("data.txt")
+//   .then((res) => res.text())
+//   .then((data) => console.log(data));
+
+fetch("data.json");
+// .then((res) => res.json())
+// .then((data) => console.log(data));
+
+const myHeaders = new Headers();
+
+const init = {
+  method: "GET",
+  headers: myHeaders,
+  mode: "cors",
+  cache: "default",
+};
+
+// fetch("data.json", init).then((res) => console.log(res));
+
+// CRUD => Create (POST), reand (GET), update (PUT), delete (delete)
